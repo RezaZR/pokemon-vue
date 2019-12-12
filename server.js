14989805667db12dd72a0c470e855dce2bd0a859
RezaZR <rezazramadhan@gmail.com>
2019-12-12 11:01:02 +0700

    fix server.js for running history router mode without backend

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require("express");
-const port = process.env.PORT || 8080;
+const path = require("path");
+const history = require("connect-history-api-fallback");
+
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
-app.listen(port);
+const staticFileMiddleware = express.static(path.join(__dirname + "/dist"));
+
+app.use(staticFileMiddleware);
+app.use(
+  history({
+    disableDotRule: true,
+    verbose: true
+  })
+);
+app.use(staticFileMiddleware);
+
+app.get("/", function(req, res) {
+  res.render(path.join(__dirname + "/dist/index.html"));
+});
+
+const server = app.listen(process.env.PORT || 8080, function() {
+  const port = server.address().port;
+  console.log("App now running on port ", port);
+});
+
+// const port = process.env.PORT || 8080;
+// app.use(express.static(__dirname + "/dist"));
+// app.listen(port);
